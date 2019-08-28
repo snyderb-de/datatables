022f66cfc2ff0696457698cacec73d8ecf81373b
Bryan Snyder <snyderb/de@gmail.com>
2019-08-28 11:32:37 -0400

    clean up
    florence lewis test page created for parent/child rows

--- testTable.js
+++ testTable.js
@@ -2,7 +2,7 @@
  * JavaScript Test File for DataTables
  * by Bryan Snyder
  * no lic.
-*/
+ */
 
 // FIXME: find an AJAX solution for local testing.
 // TODO: do we make a ready function for each Table?
@@ -13,67 +13,81 @@
 // LOOKUP: this is just a test for TODO customization
 // DEBUG: this is just a test for TODO customization
 
+jQuery(document).ready(function () {
+    // DE SAR Table
+    var table = jQuery("#deSAR-dt").DataTable({
+        ajax:
+            "https://archivesfiles.delaware.gov/dpaDataTables/deSAR.txt",
+        responsive: true,
+        columns: [
+            { data: "National #" },
+            { data: "Solider Surname" },
+            { data: "Solider First Name" },
+            { data: "Solider Suffix" },
+            { data: "State" },
+            { data: "Chapter" },
+            { data: "Service" },
+            { data: "DE Member #" },
+            { data: "DE Surname" },
+            { data: "DE First Name" },
+            { data: "DE Middle Name" },
+            { data: "DE Suffix" }
+        ],
 
-jQuery(document).ready(function() {
-  // DE SAR Table
-  var table = jQuery("#deSAR-dt").DataTable({
-    ajax: "https://archivesfiles.delaware.gov/dpaDataTables/deSAR.txt",
-    responsive: true,
-    columns: [
-      {
-        data: "National #"
-      },
-      {
-        data: "Solider Surname"
-      },
-      {
-        data: "Solider First Name"
-      },
-      {
-        data: "Solider Suffix"
-      },
-      {
-        data: "State"
-      },
-      {
-        data: "Chapter"
-      },
-      {
-        data: "Service"
-      },
-      {
-        data: "DE Member #"
-      },
-      {
-        data: "DE Surname"
-      },
-      {
-        data: "DE First Name"
-      },
-      {
-        data: "DE Middle Name"
-      },
-      {
-        data: "DE Suffix"
-      }
-    ],
-    order: [[0, "asc"]],
-    columnDefs: [
-      {
-        type: "html-num-fmt",
-        targets: [1]
-      }
-    ]
-  });
+        order: [[0, "asc"]],
 
-  //keep this inside the doc ready for this table to isolate the function
-  //
-  jQuery(function(e){
+        columnDefs: [
+            {
+            type: "html-num-fmt",
+            targets: [1]
+            }
+        ]
+    });
+    // keep this inside the doc ready for this table to isolate the function
+    //
+    jQuery(function(e) {
     jQuery.fn.DataTable.tables({
         visible: true,
         api: true
     }).column.adjust();
-  });
-  // Insert another table below
-  // ending bracket below here
+    });
+});
+
+jQuery(document).ready(function () { // Florence Lewis Table
+    var table = jQuery("#florence-lewis-dt").DataTable({
+        ajax: "https://archivesfiles.delaware.gov/dpaDataTables/9200_L08_003_Florence_Lewis.txt",
+        responsive: true,
+        columns: [
+            {data: "ID"},
+            {data: "Document Type"},
+            {data: "Buyer Last Name"},
+            {data: "Buyer First Name"},
+            {data: "Owner Last Name"},
+            {data: "Owner First Name"},
+            {data: "County"},
+            {data: "Hundred"},
+            {data: "Town"},
+            {data: "Property Description"},
+            {data: "Year"},
+            {data: "Box"},
+            {data: "Folder"}
+        ],
+        order: [
+            [0, "asc"]
+        ],
+
+        columnDefs: [{
+            type: "html-num-fmt",
+            targets: [1]
+        }]
+    });
+
+    // keep this inside the doc ready for this table to isolate the function
+    //
+    jQuery(function (e) {
+        jQuery.fn.DataTable.tables({
+            visible: true,
+            api: true
+        }).column.adjust();
+    });
 });
