5b3c692cec9b71ec1d7721b6f8a914c0b92f70f0
Bryan Snyder <snyderb/de@gmail.com>
2019-08-29 13:49:12 -0400

    Bible finished and Row Collection added

--- testTable.js
+++ testTable.js
@@ -17,32 +17,32 @@ jQuery(document).ready(function () {
     // DE SAR Table
     //function test
     var table = jQuery("#deSAR-dt").DataTable({
-        ajax:
-            "https://archivesfiles.delaware.gov/dpaDataTables/deSAR.txt",
-        responsive: true,
-        columns: [
-            { data: "National #" },
-            { data: "Solider Surname" },
-            { data: "Solider First Name" },
-            { data: "Solider Suffix" },
-            { data: "State" },
-            { data: "Chapter" },
-            { data: "Service" },
-            { data: "DE Member #" },
-            { data: "DE Surname" },
-            { data: "DE First Name" },
-            { data: "DE Middle Name" },
-            { data: "DE Suffix" }
-        ],
+      ajax: "https://archivesfiles.delaware.gov/dpaDataTables/deSAR.txt",
+      responsive: true,
+      scrollY: false,
+      columns: [
+        { data: "National #" },
+        { data: "Solider Surname" },
+        { data: "Solider First Name" },
+        { data: "Solider Suffix" },
+        { data: "State" },
+        { data: "Chapter" },
+        { data: "Service" },
+        { data: "DE Member #" },
+        { data: "DE Surname" },
+        { data: "DE First Name" },
+        { data: "DE Middle Name" },
+        { data: "DE Suffix" }
+      ],
 
-        order: [[0, "asc"]],
+      order: [[0, "asc"]],
 
-        columnDefs: [
-            {
-            type: "html-num-fmt",
-            targets: [1]
-            }
-        ]
+      columnDefs: [
+        {
+          type: "html-num-fmt",
+          targets: [1]
+        }
+      ]
     });
     // keep this inside the doc ready for this table to isolate the function
     jQuery(function(e) {
@@ -58,31 +58,33 @@ jQuery(document).ready(function () {
     // function test
 
     var table = jQuery("#florence-lewis-dt").DataTable({
-        ajax: "https://archivesfiles.delaware.gov/dpaDataTables/9200_L08_003_Florence_Lewis.txt",
-        responsive: true,
-        columns: [
-            {data: "ID"},
-            {data: "Document Type"},
-            {data: "Buyer Last Name"},
-            {data: "Buyer First Name"},
-            {data: "Owner Last Name"},
-            {data: "Owner First Name"},
-            {data: "County"},
-            {data: "Hundred"},
-            {data: "Town"},
-            {data: "Property Description"},
-            {data: "Year"},
-            {data: "Box"},
-            {data: "Folder"}
-        ],
-        order: [
-            [0, "asc"]
-        ],
+      ajax:
+        "https://archivesfiles.delaware.gov/dpaDataTables/9200_L08_003_Florence_Lewis.txt",
+      responsive: true,
+      scrollY: false,
+      columns: [
+        { data: "ID" },
+        { data: "Document Type" },
+        { data: "Buyer Last Name" },
+        { data: "Buyer First Name" },
+        { data: "Owner Last Name" },
+        { data: "Owner First Name" },
+        { data: "County" },
+        { data: "Hundred" },
+        { data: "Town" },
+        { data: "Property Description" },
+        { data: "Year" },
+        { data: "Box" },
+        { data: "Folder" }
+      ],
+      order: [[0, "asc"]],
 
-        columnDefs: [{
-            type: "html-num-fmt",
-            targets: [1]
-        }]
+      columnDefs: [
+        {
+          type: "html-num-fmt",
+          targets: [1]
+        }
+      ]
     });
 
     // keep this inside the doc ready for this table to isolate the function
@@ -101,12 +103,41 @@ jQuery(document).ready(function() {
   var table = jQuery("#bible-records-dt").DataTable({
     ajax: "https://archivesfiles.delaware.gov/dpaDataTables/bible-records.txt",
     responsive: true,
+    scrollY: false,
     columns: [
       { data: "Family Name" },
       { data: "B/C #" },
       { data: "Bible #" },
       { data: "Vol & Pg #" },
       { data: "Location" }
+    ],
+
+    order: [[0, "asc"]],
+    columnDefs: [{ type: "html-num-fmt", targets: [1] }]
+  });
+
+  // keep this inside the doc ready for this table to isolate the function
+  jQuery(function(e) {
+    jQuery.fn.DataTable.tables({
+      visible: true,
+      api: true
+    }).column.adjust();
+  });
+});
+
+jQuery(document).ready(function() {
+  // Row Collection Records
+  // function test
+  var table = jQuery("#row-collection-dt").DataTable({
+    ajax: "https://archivesfiles.delaware.gov/dpaDataTables/rowCollection.txt",
+    responsive: true,
+    scrollY: false,
+    columns: [
+      { data: "ID" },
+      { data: "Subject" },
+      { data: "Sub-Category" },
+      { data: "Year" },
+      { data: "Barcode" }
     ],
 
     order: [[0, "asc"]],
